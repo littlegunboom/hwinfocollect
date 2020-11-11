18cea80969bc74b926d3a24cfcb55d673b74f78a
littlegunboom <48616658+littlegunboom@users.noreply.github.com>
2020-11-11 13:37:13 +0800

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -8,9 +8,10 @@ if [ -f /root/${sn}_cpu_linpack.log ];then
   mv /root/${sn}_cpu_benchmark /root/${sn}_cpu_benchmark.bak
 fi  
 
-wget https://agora-devops-public-2.oss-cn-beijing.aliyuncs.com/Stress-testing/compressed_tools/linpack_2020.2.001.tar -P ~/linkpack/
-cd ~/linkpack/linpack
+wget https://agora-devops-public-2.oss-cn-beijing.aliyuncs.com/Stress-testing/compressed_tools/linpack_2020.2.001.tar -O ~/linkpack/linpack_2020.2.001.tar
+cd ~/linkpack/
 tar -xvf linpack_2020.2.001.tar 1>/dev/null 2>&1
+cd ~/linkpack/linkpack/
 sudo ./runme_xeon64 > /root/${sn}_cpu_linpack.log
 
 wget https://agora-devops-public-2.oss-cn-beijing.aliyuncs.com/Stress-testing/compressed_tools/mlc_v3.9.tgz -P ~/intel-mlc
